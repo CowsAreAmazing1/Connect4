f5a8fc9cb3ca29ec9d9bd3bcad7169de4eb1f12a
Ryan Henery <ryan.henery@hotmail.com>
2024-09-15 15:49:27 +0100

    Almost drawing the board observably

--- Board.jl
+++ Board.jl
@@ -1,21 +1,29 @@
 using GLMakie
 
-poses = [ Point2(100x - 50.0, 100y - 50.0) for x in 1:7, y in 1:6 ]
-
-
+const Board = Matrix{Symbol}
 c4red = RGBf(0.976470588235,0.121568627451,0.21568627451);
 c4yellow = RGBf(0.980392156863,0.890196078431,0.207843137255);
+c4blue = RGBf(0.1098,0.380392156863,0.9411);
+empty = 0; red = 1; yellow = 2
 
 
-f = Figure(size = (800,800))
-ax = Axis(f[1,1], aspect = DataAspect())
 
-poly!(ax, Point2.([(0,0),(700,0),(700,600),(0,600)]), color = RGBf(0.1098,0.380392156863,0.9411))
-poly!.(ax, Circle.(poses, 40.0), color = :white)
 
-while true
-    poly!(ax, Circle(rand(poses), 40.0), color = rand([c4red, c4yellow]))
-    sleep(1/100)
+
+
+
+
+
+
+function drawBoard(ax:: Axis, board::Union{Board, Observable{Board}})
+    poses = [ Point2(100x - 50.0, 100y - 50.0) for x in 1:7, y in 1:6 ] # Centers for the 7x6 grid of circles
+    ob = @lift( reshape(map(x -> lift(y -> y[x] == :red ? c4red : y[x] == :yellow ? c4yellow : :white, board), eachindex($board)), 6, 7)) # Turns the observable board of chips into an observable board of observable colours which listens to the original board
+
+
+    poly!(ax, Point2.([(0,0),(700,0),(700,600),(0,600)]), color = c4blue)
+    foreach((p,c) -> begin
+        poly!(ax, Circle(p, 40.0), color = c)
+    end, poses, ob[])
+
+    return ob
 end
-
-# arc!.(ax, vec(poses), 50, -pi, pi)
---
+++ main.jl
@@ -0,0 +1,14 @@
+
+include("Board.jl")
+include("Game.jl")
+
+
+testb::Observable{Board} = Observable(fill(:empty, 6, 7))
+testb[] = map(x -> rand([:red, :yellow, :empty]), testb[])
+
+
+f = Figure(size = (800,800))
+ax = Axis(f[1,1], aspect = DataAspect()); hidedecorations!(ax); hidespines!(ax)
+
+qwe = drawBoard(ax, testb)
+
