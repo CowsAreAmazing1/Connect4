b1ad8588f067005b84319fb81dbcd67a53cf8e2f
Ryan Henery <ryan.henery@hotmail.com>
2024-09-23 11:07:36 +0100

    Better board drawer to allow piece animation later

--- Board.jl
+++ Board.jl
@@ -1,4 +1,4 @@
-using GLMakie
+using GLMakie, GeometryBasics
 
 const Board = Matrix{Symbol}
 c4red = RGBf(0.976470588235,0.121568627451,0.21568627451);
@@ -17,10 +17,14 @@ empty = 0; red = 1; yellow = 2
 
 function drawBoard(ax:: Axis, board::Union{Board, Observable{Board}})
     poses = [ Point2(100x - 50.0, 100y - 50.0) for y in 1:6, x in 1:7 ] # Centers for the 7x6 grid of circles
-    ob = @lift( reshape(map(x -> lift(y -> y[x] == :red ? c4red : y[x] == :yellow ? c4yellow : y[x] != :empty ? y[x] : :white, board), eachindex($board)), 6, 7)) # Turns the observable board of chips into an observable board of observable colours which listens to the original board
+    ob = @lift( reshape(map(x -> lift(y -> y[x] == :red ? c4red : y[x] == :yellow ? c4yellow : y[x] != :empty ? y[x] : (:white, 0.0), board), eachindex($board)), 6, 7)) # Turns the observable board of chips into an observable board of observable colours which listens to the original board
 
+    p = Polygon(
+        Point2d[(0,0),(700,0),(700,600),(0,600)],
+        vec(map(x -> collect(coordinates(Circle(x, 40))), poses))
+    );
+    poly!(ax, p, color = c4blue)
 
-    poly!(ax, Point2.([(0,0),(700,0),(700,600),(0,600)]), color = c4blue)
     foreach((p,c) -> begin
         poly!(ax, Circle(p, 40.0), color = c)
     end, poses, ob[])
--- main.jl
+++ main.jl
@@ -4,14 +4,30 @@ include("Game.jl")
 
 
 testb::Observable{Board} = Observable(fill(:empty, 6, 7))
-testb[] = map(x -> rand([:red, :yellow, :green, :blue, :empty]), testb[])
-
+testb[] = map(x -> rand([:red, :yellow, :empty]), testb[])
 
 f = Figure(size = (800,800))
 ax = Axis(f[1,1], aspect = DataAspect()); hidedecorations!(ax); hidespines!(ax)
 
 qwe = drawBoard(ax, testb)
 
-testb[][1, :]
-testb[][:, 1]
+a = @lift([ filter(y -> y != :empty, $testb[:, i]) for i in 1:7 ])
+foreach(x -> while length(a[][x]) != 6; push!(a[][x], :empty); end, eachindex(a[]))
+a = @lift( reshape(reduce(vcat, $a), 6, 7) )
+
+
+playpos = Observable(Point2f(0,0))
+sectionboxfunc = on(events(ax).mouseposition) do event
+    playpos[] = Point2f(mouseposition(ax)...)
+end
+
+poly!(ax, @lift( Circle($playpos, 40.0)), color = :red)
+
+
+
+drawBoard(ax, a)
+
+
+
+
 
