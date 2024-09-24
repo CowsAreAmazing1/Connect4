4383a5c9d8a3f12982927f709ef413db0c2a53f0
CowsAreAmazing1 <ryan.henery@hotmail.com>
2024-09-24 21:26:06 +0100

    Added an almost working piece animation handler

--- Board.jl
+++ Board.jl
@@ -1,33 +1,80 @@
-using GLMakie, GeometryBasics
+println("Importing GLMakie..")
+using GLMakie
+println("Importing GeometryBasics..")
+using GeometryBasics
+println("Done adding packages")
 
 const Board = Matrix{Symbol}
-c4red = RGBf(0.976470588235,0.121568627451,0.21568627451);
-c4yellow = RGBf(0.980392156863,0.890196078431,0.207843137255);
-c4blue = RGBf(0.1098,0.380392156863,0.9411);
+c4red = RGBAf(0.976470588235,0.121568627451,0.21568627451);
+c4yellow = RGBAf(0.980392156863,0.890196078431,0.207843137255);
+c4blue = RGBAf(0.1098,0.380392156863,0.9411);
 empty = 0; red = 1; yellow = 2
 
+colorer(x) = x == :red ? c4red : x == :yellow ? c4yellow : x != :empty ? x : RGBAf(1, 1, 1, 0.0)
 
 
+poses = [ Point2((100x - 50.0, 100y - 50.0)) for y in 1:6, x in 1:7 ] # Centers for the 7x6 grid of circles
 
-
-
-
-
-
-
-function drawBoard(ax:: Axis, board::Union{Board, Observable{Board}})
-    poses = [ Point2(100x - 50.0, 100y - 50.0) for y in 1:6, x in 1:7 ] # Centers for the 7x6 grid of circles
-    ob = @lift( reshape(map(x -> lift(y -> y[x] == :red ? c4red : y[x] == :yellow ? c4yellow : y[x] != :empty ? y[x] : (:white, 0.0), board), eachindex($board)), 6, 7)) # Turns the observable board of chips into an observable board of observable colours which listens to the original board
-
+function drawBoardPlate(ax)
     p = Polygon(
         Point2d[(0,0),(700,0),(700,600),(0,600)],
         vec(map(x -> collect(coordinates(Circle(x, 40))), poses))
     );
     poly!(ax, p, color = c4blue)
+end
+function drawBoard(ax:: Axis, board::Observable{Board})
+
+    ob = map(x -> begin
+            lift(y -> colorer(y[x]), board)
+        end, eachindex(board[]))
+     # Turns the observable board of chips into a board of observable colours which listens to the original board
+
+    drawBoardPlate(ax)
 
     foreach((p,c) -> begin
         poly!(ax, Circle(p, 40.0), color = c)
-    end, poses, ob[])
+    end, poses, ob)
 
     return ob
+end
+
+
+struct PieceMover
+    startIndex::CartesianIndex
+    endIndex::CartesianIndex
+    time::Real
+
+    PieceMover(s::CartesianIndex, e::CartesianIndex, t::Real) = begin
+        new(s, e, t)
+    end
+end
+
+Base.iterate(pm::PieceMover, state = 0) = begin
+    smoothstep(x) = x * x
+    ts(x) = smoothstep(x / 60 / pm.time)
+
+    state > 60pm.time ? nothing : (
+        (1-ts(state)) * poses[pm.startIndex] + 
+        ts(state)     * poses[pm.endIndex], state + 1
+    )
+end
+
+function animatePiece(board::Observable{Matrix{Symbol}}, p::PieceMover)
+    c = board[][p.startIndex]
+    board[][p.startIndex] = :empty
+    notify(board)
+
+    next = iterate(p)
+    pos = Observable(next[1])
+    circ = poly!(ax, @lift(Circle($pos, 40.0)), color = :green) #colorer(c))
+
+    drawBoardPlate(ax)
+
+    while next !== nothing
+        pos[], state = next
+        next = iterate(p, state)
+        sleep(1/60)
+    end
+    board[][p.endIndex] = c
+    delete!(ax, circ)
 end
--- main.jl
+++ main.jl
@@ -1,7 +1,12 @@
+println("Starting...")
+println("Adding external files...")
 
 include("Board.jl")
-include("Game.jl")
+println("Added Board.jl")
 
+include("Game.jl")
+println("Added Game.jl")
+println("Done! Running main...")
 
 testb::Observable{Board} = Observable(fill(:empty, 6, 7))
 testb[] = map(x -> rand([:red, :yellow, :empty]), testb[])
@@ -9,25 +14,97 @@ testb[] = map(x -> rand([:red, :yellow, :empty]), testb[])
 f = Figure(size = (800,800))
 ax = Axis(f[1,1], aspect = DataAspect()); hidedecorations!(ax); hidespines!(ax)
 
-qwe = drawBoard(ax, testb)
+q = drawBoard(ax, testb)
 
-a = @lift([ filter(y -> y != :empty, $testb[:, i]) for i in 1:7 ])
+
+shifts = [ findall(x -> x == :empty, testb[][:, i]) for i in 1:7 ]
+
+movers = []
+for x in 1:7
+    for y in 1:6
+        y in shifts[x] && continue
+        ct = count(z -> z < y, shifts[x])
+        ct == 0 && continue
+
+        st = CartesianIndex(y, x)
+        en = CartesianIndex(y - ct, x)
+        push!(movers, PieceMover(st, en, 0.3))
+    end
+end
+
+display(f)
+
+println("gonna threads soon")
+sleep(5)
+
+Threads.@threads :static for i in movers
+    sleep(5rand())
+    animatePiece(testb, i)
+end
+
+
+#=
+foreach(x -> begin
+    c = testb[][x.startIndex]
+    testb[][x.startIndex] = :empty
+
+    next = iterate(x)
+    pos = Observable(next[1])
+    poly!(ax, @lift(Circle($pos, 40.0)), color = colorer(c))
+
+    Threads.@spawn while next !== nothing
+        pos[], state = next
+        next = iterate(x, state)
+        sleep(1/60)
+    end
+
+
+end, movers); notify(testb)
+
+
+foreach(x -> begin
+    c = testb[][x.startIndex]
+    testb[][x.startIndex] = :empty
+    testb[][x.endIndex]   = c
+end, movers)
+notify(testb)
+
+
+movers
+
+
+testb = @lift([ filter(y -> y != :empty, $testb[:, i]) for i in 1:7 ])
 foreach(x -> while length(a[][x]) != 6; push!(a[][x], :empty); end, eachindex(a[]))
 a = @lift( reshape(reduce(vcat, $a), 6, 7) )
 
 
-playpos = Observable(Point2f(0,0))
+playpos = Observable(Point2f((0,0)))
 sectionboxfunc = on(events(ax).mouseposition) do event
     playpos[] = Point2f(mouseposition(ax)...)
 end
 
 poly!(ax, @lift( Circle($playpos, 40.0)), color = :red)
 
-
-
 drawBoard(ax, a)
 
 
 
 
+pos1 = CartesianIndex(1,1)
+pos2 = CartesianIndex(3,2)
+pm = PieceMover(pos1, pos2, 1)
 
+pos = Observable(poses[pos1])
+next = iterate(pm)
+
+f, ax = scatter(poses[pos2])
+scatter!(ax, poses[pos1]); scatter!(ax, pos)
+ax.limits = (0, 700, 0, 600)
+
+while next !== nothing
+    pos[], state = next
+    next = iterate(pm, state)
+    sleep(1/60)
+end
+
+=#
